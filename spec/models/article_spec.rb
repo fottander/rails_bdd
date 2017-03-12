3ab0c99efe1bf3e4aa367d19576a45a58b90b2ca
felixottander <felix.ottander@yahoo.com>
2017-03-12 15:27:42 +0100

    Adding associations in article spec, also adding the association in the article model.

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -6,6 +6,10 @@ RSpec.describe Article, type: :model do
     it { is_expected.to have_db_column :content }
   end
 
+  describe 'Associations' do
+    it { is_expected.to have_many :comment }
+  end
+
   describe 'Validations' do
     it { is_expected.to validate_presence_of :title }
     it { is_expected.to validate_presence_of :content }
