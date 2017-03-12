4f66987445d796654dbad79c07349cce2a85beac
felixottander <felix.ottander@yahoo.com>
2017-03-12 15:52:28 +0100

    Adding the create method in comments controller.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,4 +3,9 @@ class CommentsController < ApplicationController
     @article = Article.find(params[:article_id])
     @comment = Comment.new(article: @article)
   end
+  def create
+    binding.pry
+    @article = Article.find(params[:article_id])
+    @comment = Comment.new(article: @article, email: params[:comment][:email], comment: params[:comment][:comment])
+  end
 end
