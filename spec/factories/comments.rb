db6ae226f5e55dd5cc2f0b86daeecd41fa0c67a7
felixottander <felix.ottander@yahoo.com>
2017-03-12 15:22:45 +0100

    Changing the comment_spec to be in the spec models folder. Removing nil from factory file, article line.

--- spec/factories/comments.rb
+++ spec/factories/comments.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :comment do
-    email ""
+    email "MyEmail"
     comment "MyText"
-    article nil
+    article 
   end
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'DB columns' do
+    it { is_expected.to have_db_column :email }
+    it { is_expected.to have_db_column :comment }
+  end
+
+  describe 'Associations' do
+    it { is_expected.to belong_to :article }
+  end
+
+  describe 'Comment Factory' do
+    it 'Should have valid Factory' do
+      expect(FactoryGirl.create(:comment)).to be_valid
+    end
+  end
 end
