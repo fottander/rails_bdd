e3670eed17620e6b42f48144b42ce4bea65b8d36
felixottander <felix.ottander@yahoo.com>
2017-03-12 10:02:41 +0100

    Adding second test to see text on articles page. Create variable for finding article in controller. Also adding the html in the view to display title and publishing date.

--- features/article_page.feature
+++ features/article_page.feature
@@ -10,3 +10,4 @@ Feature: An article has comments
 
   Scenario: Create a new comment for the article
     Given I visit the "Mucho gracias" article page
+    Then I should see "Mucho gracias" and publishing date "2016-05-24"
--- features/step_definitions/article_page.rb
+++ features/step_definitions/article_page.rb
@@ -8,3 +8,7 @@ Given(/^I visit the "([^"]*)" article page$/) do |article_title|
   article = Article.where(title: article_title).first
   visit article_path(article)
 end
+
+Then(/^I should see "([^"]*)" and publishing date "([^"]*)"$/) do |content, date|
+  expect(page).to have_content and date
+end
