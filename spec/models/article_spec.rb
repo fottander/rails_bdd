11a6c5df0edd2f6cab95a62e0681bc544a139b15
felixottander <felix.ottander@yahoo.com>
2017-03-18 19:32:21 +0100

    Adds column to author spec and author in factory.

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe Article, type: :model do
     it { is_expected.to have_db_column :id }
     it { is_expected.to have_db_column :title }
     it { is_expected.to have_db_column :content }
+    it { is_expected.to have_db_column :author }
   end
 
   describe 'Associations' do
