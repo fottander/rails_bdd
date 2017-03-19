cad7b9290331f6a7cfae41c9d262b6aae0b9a12c
felixottander <felix.ottander@yahoo.com>
2017-03-19 13:32:19 +0100

    Adds email formatting and an error if email is incorrect.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,11 +4,17 @@ class CommentsController < ApplicationController
     @comment = Comment.new(article: @article)
   end
   def create
+    @email = (params[:comment][:email] =~ /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i)
+    if @email == nil
+      flash[:alert] = "WRONG EMAIL FORMAT!"
+      @article = Article.find(params[:article_id])
+      redirect_to article_path(@article)
+    else
     @article = Article.find(params[:article_id])
     @comment = Comment.new(article: @article, email: params[:comment][:email], comment: params[:comment][:comment])
-
     if @comment.save
       redirect_to article_path(@article)
+    end
     end
   end
 end
